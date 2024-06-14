94a09f91ae4a7b1383efaf4105bb14af7431246e
webploy99 <webploy99@gmail.com>
2024-06-14 12:48:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+function OpenNav() {
+    document.getElementById("Nav").style.width = "100%";
+}
+  function CloseNav() {
+    document.getElementById("Nav").style.width = "0";
+}
+
+// url
+function url() {
+    location.href = "https://www.hay8833.com/?inviteCode=3128320";
+  }
+
+var swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    slidesPerView: 3,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94
+    },
+    breakpoints: {
+        350: {
+            slidesPerView: 1,
+            resistanceRatio: 0.85
+        },
+        980: {
+            slidesPerView: 3,
+            resistanceRatio: 0.85
+        },
+    },
+
+
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+            const currentSlide = s.slides[s.activeIndex];
+            const progressBar = currentSlide.querySelector(".progressBar");
+            if (progressBar) {
+                const widthPercentage = parseFloat(progress) * 100;
+                progressBar.style.width = widthPercentage + "%";
+            }
+        }
+    }
+});
